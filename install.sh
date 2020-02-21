6786a7da2649004dce41da9a8c6192a7f1e7fd1e
james <james@modalai.com>
2020-02-21 15:06:57 -0800

    make install.sh script actually executable and work in 19.10

--- install.sh
+++ install.sh
@@ -1,18 +1,11 @@
+#! /bin/bash
+# DONT RUN AS ROOT
 
-# upgrading python 3.5 to 3.6 (to be able to use harold to get cont tf fro discrete)
-sudo add-apt-repository ppa:jonathonf/python-3.6
+set -e
 
-sudo apt-get update
+sudo apt install libgeos-3* libgeos-dev python3-tk gfortran cmake libssl-dev libblas-dev liblapack-dev -y
 
-sudo apt-get install python3.6
-
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo apt-get install python3.6-dev
-
-# Needed for px4tools
-sudo apt-get install libgeos-3.5.0
-sudo apt-get install libgeos-dev
-sudo pip install https://github.com/matplotlib/basemap/archive/master.zip
+pip3 install https://github.com/matplotlib/basemap/archive/master.zip
 
 pip3 install numpy
 pip3 install px4tools
@@ -22,21 +15,22 @@ pip3 install harold
 # Dependencies for SIPPY
 pip3 install control
 pip3 install scipy
+
 # needed for slycot
 pip3 install scikit-build
 pip3 install slycot
 pip3 install future
 pip3 install matplotlib
-# Needed to be able to "show" plots by matplotlib
-sudo apt-get install python3-tk
-
-# Install SIPPY from github
-git clone https://github.com/CPCLAB-UNIPI/SIPPY.git
-cd SIPPY
-python3 setup.py install
-# or, in case of permission error
-sudo python setup.py install
 
 # Genetic algorithm package
-pip install deap
-# or sudo pip install deap
+pip3 install deap
+
+
+# Install SIPPY from github
+git submodule init
+git submodule update
+cd SIPPY
+python3 setup.py install
+cd -
+
+
